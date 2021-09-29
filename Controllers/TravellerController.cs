00b5f4d622e3888325cc28770cc94c5a5fc3955e
zulyhallo <zuleyhaokur54@gmail.com>
2021-09-29 07:53:57 +0300

    ApiCrudwithSql

---
+++ Controllers/TravellerController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace CRUD_API5.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TravellerController : ControllerBase
+    {
+
+        private readonly IConfiguration _configuration;
+        public TravellerController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+       
+        //API Methods
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"
+                           select TravellerID,TravellerName,TravellerCity,TravellerAge from 
+                           dbo.Traveller
+                           "; // sql komutunu query'e atma
+            DataTable table = new DataTable(); // table nesnesi oluturma 
+            string sqlDataSource = _configuration.GetConnectionString("TravellerAppCon");//sql bağlantısı 
+            SqlDataReader myReader; //get komutu için sqlden verileri alma metodu(sql komutu)
+            using (SqlConnection myCon= new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();// bağlantıyı açma
+                using(SqlCommand myCommand=new SqlCommand(query, myCon)) // mycona bağlanıp query'i çalıştırması için
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close(); // bağlantıları kapatma 
+                    myCon.Close();  
+                }
+            }
+            return new JsonResult(table);
+
+        }
+    }
+}
---
+++ Models/Traveller.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD_API5.Models
+{
+    public class Traveller
+    {
+        public int ID { get; set; }
+        public string name { get; set; }
+        public string city { get; set; }
+        public int age { get; set; }
+    }
+}
